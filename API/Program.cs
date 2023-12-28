94573d90a302c9380dcec711b2ffc9448d806814
Vinay <Vinay@DESKTOP-7GG6AMC>
2023-12-29 00:11:06 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -6,21 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(corpolicybuilder => corpolicybuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
